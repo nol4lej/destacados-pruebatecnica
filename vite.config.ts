aa93541e73dd01ec1fa991fb839a2de905bee0a6
nol4lej <nome.ahumada@outlook.com>
2024-06-13 18:36:55 -0400

    Agregados alias de importacion para directorios: components, services, styles y utils

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@utils': path.resolve(__dirname, './src/utils'),
     }
   }
 })
