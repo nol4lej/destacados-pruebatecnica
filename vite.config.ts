67f5c4a851b8434a130dcd586dd01ec1936ce41e
nol4lej <nome.ahumada@outlook.com>
2024-06-13 19:17:42 -0400

    Alias para directorio hooks agregado

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       '@services': path.resolve(__dirname, './src/services'),
       '@styles': path.resolve(__dirname, './src/styles'),
       '@utils': path.resolve(__dirname, './src/utils'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
     }
   }
 })
