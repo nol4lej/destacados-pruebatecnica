a65201ee19272355120b5a3608d3ef99b3a7dbaa
nol4lej <nome.ahumada@outlook.com>
2024-06-14 04:24:19 -0400

    feat: Implementación del hook useApi para manejo de solicitudes API
    - Se ha implementado el hook useApi que permite enviar solicitudes HTTP utilizando Axios y gestionar el estado de respuesta.
    - El hook utiliza useState para mantener el estado local de los datos de respuesta, errores y el estado de carga.
    - Incluye la función sendRequest que envía una solicitud API utilizando la instancia configurada de Axios.
    - La función captura y maneja tanto los datos de respuesta exitosa como los errores, actualizando el estado correspondiente en consecuencia.

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+import api from '@services/api';
+import { ErrorResponseOptions, ApiResponse, UseApiReturnType } from '@interfaces/api';
+
+const useApi = (): UseApiReturnType => {
+  // Estado local para almacenar la respuesta, error y estado de carga
+  const [response, setResponse] = useState<UseApiReturnType['response']>({
+    data: null,
+    error: null,
+    loading: false
+  });
+
+  // Función para enviar una solicitud API
+  const sendRequest = async (config: AxiosRequestConfig) => {
+    setResponse({ data: null, error: null, loading: true }); // Iniciar estado de carga
+
+    try {
+      // Enviar la solicitud API utilizando la instancia configurada de Axios (por defecto es GET)
+      const response: AxiosResponse<ApiResponse> = await api.request<ApiResponse>(config);
+
+      console.log(response.data);
+
+      // Actualizar el estado con los datos recibidos y sin errores
+      setResponse({ data: response.data, error: null, loading: false });
+    } catch (error) {
+
+      console.error(error);
+
+      // Actualizar el estado con error capturado y sin datos nuevos
+      setResponse({ data: null, error: error as AxiosError<ErrorResponseOptions>, loading: false });
+    }
+  };
+
+  // Devolver el estado actualizado y la función para enviar solicitudes API
+  return { response, sendRequest };
+};
+
+export default useApi;
