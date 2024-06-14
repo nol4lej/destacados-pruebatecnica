ce9a4aff60092568ff54e762381704ae33d68ee6
nol4lej <nome.ahumada@outlook.com>
2024-06-14 13:49:07 -0400

    feat: Agregar componente HomePage a la aplicación principal
    - Se ha importado y utilizado el componente HomePage dentro de la función principal App.
    - Los estilos CSS de la aplicación se han organizado en el archivo App.css dentro del directorio de estilos.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css'
+import HomePage from './pages/Home'; // Ajusta la ruta según la estructura de tu proyecto
+import '@styles/App.css';
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <div className="app-container">
+      <HomePage />
+    </div>
+  );
 }
 
-export default App
+export default App;
