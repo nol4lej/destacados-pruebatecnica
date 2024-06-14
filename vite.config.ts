17d0529b4c84d037eaaa7d89f92305a1f015db88
nol4lej <nome.ahumada@outlook.com>
2024-06-14 03:36:39 -0400

    Añadido alias de interfaces

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       '@styles': path.resolve(__dirname, './src/styles'),
       '@utils': path.resolve(__dirname, './src/utils'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@interfaces': path.resolve(__dirname, './src/interfaces'),
     }
   }
 })
