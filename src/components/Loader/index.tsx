754369abda980dcbbb0491e719ea4d1f40dbe738
nol4lej <nome.ahumada@outlook.com>
2024-06-14 15:11:35 -0400

    feat: componente Loader para mostrar un indicador de carga.
    - Utiliza un spinner circular con borde y animación CSS.
    - Centra el spinner dentro del contenedor con flexbox.

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+const Loader = () => {
+    return (
+        <div className="loader-container">
+            <div className="loader-spinner"></div>
+        </div>
+    );
+}
+
+export default Loader;
