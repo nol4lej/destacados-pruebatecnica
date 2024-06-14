d7afdd4cd27dc68256bbcef0f0218fbad6fd1eeb
nol4lej <nome.ahumada@outlook.com>
2024-06-14 02:59:45 -0400

    feat: Configurar interceptores de respuestas y errores con tipos específicos
    Este commit introduce la configuración de interceptores para manejar respuestas y errores en una instancia de Axios. Se importan las funciones de manejo de respuestas (handleResponse) y errores (handleError) desde './handlers', junto con la interfaz ErrorResponseOptions para tipar correctamente los errores manejados por Axios

---
+++ src/services/api/interceptors.ts
@@ -0,0 +1,20 @@
+import { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
+import { handleResponse, handleError, ErrorResponseOptions } from './handlers';  // Importamos las funciones de manejo de respuestas y errores desde './handlers', y la interfaz 'ErrorResponseOptions' para tipar AxiosError
+
+// Función para configurar interceptores de respuestas y errores
+export const setupInterceptors = (instance: AxiosInstance) => {
+
+    // Configurar interceptor de respuestas
+    instance.interceptors.response.use(
+
+        (response: AxiosResponse) => {
+            // Llamar a la función de manejo de respuestas exitosas
+            return handleResponse(response);
+        },
+
+        (error: AxiosError<ErrorResponseOptions>) => {
+            // Llamar a la función de manejo de errores
+            return handleError(error);
+        }
+    );
+};
