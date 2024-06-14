2b01a0fc61a31d7e27d806e387502e01fa446021
nol4lej <nome.ahumada@outlook.com>
2024-06-14 16:29:56 -0400

    refact: ruta de logo cambiada a public.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,11 @@
 import CharacterGallery from "@components/CharacterGallery";
-import logo from "@assets/logo.svg";
 import './index.css';
 
 const HomePage = () => {
     return (
         <div className="homepage-container">
             <div className="logo-container">
-                <img src={logo} alt="Logo de la página" className="logo-img" />
+                <img src="/logo.svg" alt="Logo de la página" className="logo-img" />
             </div>
             <CharacterGallery />
         </div>
