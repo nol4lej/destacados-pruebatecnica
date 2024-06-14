0a839b38235bf551cd354edf0ec187db1493defc
nol4lej <nome.ahumada@outlook.com>
2024-06-14 03:01:21 -0400

    refactor: Consolidar interceptores en un solo interceptor en index.ts
    Este commit refactoriza el código en index.ts para consolidar los interceptores de respuestas y errores en un solo interceptor. Ahora se utiliza una única configuración de interceptor para manejar tanto las respuestas exitosas como los errores de Axios de manera más eficiente y centralizada.

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -5,10 +5,9 @@
 // Importación de la instancia de Axios configurada
 import api from './instance';
 // Importación de funciones para configurar interceptores
-import { setupResponseInterceptor, setupErrorInterceptor } from './interceptors';
+import { setupInterceptors,  } from './interceptors';
 
 // Configuración de interceptores para la instancia de Axios
-setupResponseInterceptor(api); // Configura el interceptor para gestionar las respuestas
-setupErrorInterceptor(api); // Configura el interceptor para gestionar los errores
+setupInterceptors(api); // Configura el interceptor para gestionar las respuestas y errores
 
 export default api; // Exporta la instancia de Axios configurada y lista para ser utilizada
